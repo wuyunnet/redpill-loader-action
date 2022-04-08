7da23bfdcb505bc84868d724124ff4a7cdbf3716
wuyunnet <wuyunnet@163.com>
2022-04-08 14:40:42 +0800

    Update build-ds918-7.0.1.sh

--- build-ds918-7.0.1.sh
+++ build-ds918-7.0.1.sh
@@ -10,7 +10,7 @@ cd ds918-7.0.1
 
 # download redpill
 git clone --depth=1 https://github.com/RedPill-TTG/redpill-lkm.git
-git clone -b develop --depth=1 https://github.com/jumkey/redpill-load.git
+git clone -b develop --depth=1 https://github.com/RedwinX/redpill-load.git
 
 # download syno toolkit
 curl --location "https://sourceforge.net/projects/dsgpl/files/toolkit/DSM7.0/ds.apollolake-7.0.dev.txz/download" --output ds.apollolake-7.0.dev.txz
